a7a24b055e6000030c78c10d344aa5856b04de54
ross96D <abelaross96@gmail.com>
2023-10-18 13:18:14 -0400

    minor shit

--- cmd/root.go
+++ cmd/root.go
@@ -14,13 +14,7 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "mclip",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Copy the stdin output to the user clipboard, maybe some more functionalities will be added later",
 	Run: func(cmd *cobra.Command, args []string) {
 		fromStdin()
 	},
